920699ff9a73d2bf6c17e856043a3d75cb495007
aruipalEEAE <aruipal@eeae.local>
2025-02-24 08:49:17 +0100

    first commit

--- Tabla multiplicar.py
+++ Tabla multiplicar.py
@@ -1,7 +1,7 @@
 #Tabla de multiplicar del 1 al 10 de un número dado.
 
 def tabla(num):
-    print(f"Tabla del {num}")
+    print(f"Tabla del {num}:")
     for i in range(1,11):
         print(f"{num}x{i}={num*i}") #print(num + "x" + i + "=" + (num*i))
 
