8f7b624faf66a553680eb6db8ff45cf80a971003
aruipalEEAE <aruipal@eeae.local>
2025-02-25 13:54:33 +0100

    quinto commit

--- Concurso.py
+++ Concurso.py
@@ -14,6 +14,7 @@ class concurso:
     def concursar(self, iteraciones):
         self.iteraciones = iteraciones
         for i in range(0, iteraciones):
+            self.puertas = ["A", "B", "C"]
             eleccion_jugador = random.choice(self.puertas)
             puerta_premio = random.choice(self.puertas)
 
@@ -27,7 +28,7 @@ class concurso:
                 fase_final.append(eleccion_jugador)
                 fase_final.append(puerta_premio)
             
-            cambio = random.radint(0,1)
+            cambio = random.randint(0,1)
 
             if cambio == 0:
                 fase_final.remove(eleccion_jugador)
@@ -43,10 +44,10 @@ class concurso:
                     self.pierde_con_cambio += 1
     
     def informe(self):
-        print(f"Cambiando la caja el jugador ha:  \n    -Ganado el {(self.gana_con_cambio/self.iteraciones)*100}% de las veces. \n    -Perdido el {(self.pierde_con_cambio/self.iteraciones)*100}% de las veces. \n No cambiando:\n -Ganado el {(self.gana_sin_cambio/self.iteraciones)*100}\n   -Perdido el {(self.pierde_con_cambio/self.iteraciones)*100}% de las veces.")           
+        print(f"Cambiando la caja el jugador ha:  \n    -Ganado el {(self.gana_con_cambio/self.iteraciones)*100}% de las veces. \n    -Perdido el {(self.pierde_con_cambio/self.iteraciones)*100}% de las veces. \n No cambiando:\n   -Ganado el {(self.gana_sin_cambio/self.iteraciones)*100}\n   -Perdido el {(self.pierde_con_cambio/self.iteraciones)*100}% de las veces.")           
 
 concurso1 = concurso()
 concurso1.concursar(100)
-
+concurso1.informe()
 
 
